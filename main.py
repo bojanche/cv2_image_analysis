089c7bc74300c3a4646ed291433cea7b0d60816e
Bojan <32612389+bojanche@users.noreply.github.com>
2023-07-17 17:55:14 +0200

    Update main.py
    ups

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-vcap = cv.VideoCapture("rtsp://admin:Bojana2512@192.168.39.102:8554/fhd")
+vcap = cv.VideoCapture("rtsp://admin:password@192.168.39.102:8554/fhd")
 while(1):
     ret, frame = vcap.read()
     cv.imshow('VIDEO', frame)
